5ff946d00d7044ffe44b503e7882e248faf3fcf7
Carlos Lopez Perez <iamlowlo@gmail.com>
2016-10-17 23:22:59 +0200

    Added reset and variables styles files

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     sassOptions: {
-      extension: 'sass'
+      extension: 'scss'
     }
   });
 
