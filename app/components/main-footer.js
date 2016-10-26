c1f390afd73fc29e904986da52001778902202ff
Carlos Lopez Perez <iamlowlo@gmail.com>
2016-10-26 19:37:03 +0200

    Footer styles added

--- app/components/main-footer.js
+++ app/components/main-footer.js
@@ -2,5 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	tagName: "footer",
-	classNames: ["main_footer"]
+	classNames: ["main_footer"],
+	mailAddr: "iamlowlo@gmail.com"
 });
