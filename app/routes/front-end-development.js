ac10d5c05e668a240c3a3107377bc2383e4dc926
Carlos Lopez Perez <iamlowlo@gmail.com>
2016-11-26 17:48:18 +0100

    Added hover states and apearance to grid-elements-links

--- app/routes/front-end-development.js
+++ app/routes/front-end-development.js
@@ -10,35 +10,35 @@ export default Ember.Route.extend({
 					link: "section-link",
 					param: "1"
 				},
-				background: "http://fakeimg.pl/450x150"
+				background: "https://s-media-cache-ak0.pinimg.com/originals/cb/07/85/cb0785b19d5978ace690fed6bdfa9f1f.jpg"
 			},{
 				title: "Bq",
 				link: {
 					link: "section-link",
 					param: "2"
 				},
-				background: "http://fakeimg.pl/450x150"
-			},{
-				title: "Flickr Map",
-				link: {
-					link: "section-link",
-					param: "3"
-				},
-				background: "http://fakeimg.pl/450x150"
+				background: "http://images.naldzgraphics.net/2013/06/9-turkish-triangles.jpg"
 			},{
 				title: "Honda F1",
 				link: {
 					link: "section-link",
 					param: "4"
 				},
-				background: "http://fakeimg.pl/450x150"
+				background: "https://static.vecteezy.com/system/resources/previews/000/099/823/non_2x/abstract-triangle-geometric-pattern-background-vector.jpg"
+			},{
+				title: "Flickr Map",
+				link: {
+					link: "section-link",
+					param: "3"
+				},
+				background: "https://s-media-cache-ak0.pinimg.com/236x/13/4f/f9/134ff98c12edda3d8a179f6819cabeb3.jpg"
 			},{
 				title: "La Poste",
 				link: {
 					link: "section-link",
 					param: "5"
 				},
-				background: "http://fakeimg.pl/450x150"
+				background: "https://s-media-cache-ak0.pinimg.com/736x/e2/7e/d5/e27ed5dbeef43ab18c736e217411c7ff.jpg"
 			}]
 		};
 	}
--- app/routes/graphic-design.js
+++ app/routes/graphic-design.js
@@ -1,4 +1,52 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model() {
+		return {
+			title: 'Graphic Design',
+			feed: [{
+				title: "PQ Series",
+				link: {
+					link: "section-link",
+					param: "6"
+				},
+				background: "http://fakeimg.pl/450x150"
+			},{
+				title: "Gifts",
+				link: {
+					link: "section-link",
+					param: "7"
+				},
+				background: "http://fakeimg.pl/450x150"
+			},{
+				title: "Curious Wines",
+				link: {
+					link: "section-link",
+					param: "8"
+				},
+				background: "http://fakeimg.pl/450x150"
+			},{
+				title: "Hedphones Area",
+				link: {
+					link: "section-link",
+					param: "9"
+				},
+				background: "http://fakeimg.pl/450x150"
+			},{
+				title: "Memory Power",
+				link: {
+					link: "section-link",
+					param: "10"
+				},
+				background: "http://fakeimg.pl/450x150"
+			},{
+				title: "Sleep Well",
+				link: {
+					link: "section-link",
+					param: "11"
+				},
+				background: "http://fakeimg.pl/450x150"
+			}]
+		};
+	}
 });
