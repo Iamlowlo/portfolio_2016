c47c891e604ac5bfacecc92b1e5b4bb53e12f73e
Carlos Lopez Perez <iamlowlo@gmail.com>
2016-12-06 19:12:49 +0100

    Added test for photo-gallery

--- tests/integration/components/photo-gallery-test.js
+++ tests/integration/components/photo-gallery-test.js
@@ -6,19 +6,39 @@ moduleForComponent('photo-gallery', 'Integration | Component | photo gallery', {
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{photo-gallery}}`);
+  this.set('photo_id', 0);
+  this.set('model', [{
+      id: 0,
+      urlList: {
+        thumbnail: 'http://fakeimg.pl/100x100/4589C1/333',
+        screen_sm: 'http://fakeimg.pl/320x180/4589C1/333',
+        screen_md: 'http://fakeimg.pl/640x360/4589C1/333',
+        screen_lg: 'http://fakeimg.pl/1280x720/4589C1/333',
+        screen_xlg: 'http://fakeimg.pl/1920x1080/4589C1/333'
+      },
+      title: 'test_0'
+    },{
+      id: 1,
+      urlList: {
+        thumbnail: 'http://fakeimg.pl/100x100/0B7F6C/333',
+        screen_sm: 'http://fakeimg.pl/320x180/0B7F6C/333',
+        screen_md: 'http://fakeimg.pl/640x360/0B7F6C/333',
+        screen_lg: 'http://fakeimg.pl/1280x720/0B7F6C/333',
+        screen_xlg: 'http://fakeimg.pl/1920x1080/0B7F6C/333'
+      },
+      title: 'test_1'
+    }]);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
   this.render(hbs`
-    {{#photo-gallery}}
+    {{#photo-gallery photoList=model selectedId=photo_id}}
       template block text
     {{/photo-gallery}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text()
+      .replace(/[\r\n]+/g,' ')
+      .replace(/\s{2,}/g,' ')
+      .trim(), 'test_0 open test_0 test_1', "It renders properly and doesn't include any yield");
+
 });
